6ea4ba92c37123cffd0f7a92ea538c5a0a595ee7
James_C <a9356070@gmail.com>
2020-11-28 15:56:48 +0800

    Duration done,but still don't know how to test Method:POST

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const app = express()
 const port = 3000
 
 app.use(function (req, res, next) {
-  const time = new Date()
-  const year = time.getFullYear()
-  const month = time.getMonth() + 1
-  const localTime = time.toLocaleTimeString()
+  const start_time = new Date()
+  const year = start_time.getFullYear()
+  const month = start_time.getMonth() + 1
+  const localTime = start_time.toLocaleTimeString()
   const reqUrl = req.url
   const reqMethod = req.method
 
-  console.log(`${year}-${month}-${time.getDate()} ${localTime} | ${reqMethod} from ${reqUrl}`)
+  res.on('finish', () => {
+    const end_time = new Date()
+    const duration = end_time - start_time
+
+    console.log(`${year}-${month}-${start_time.getDate()} ${localTime} | ${reqMethod} from ${reqUrl} | total time: ${duration}ms`)
+  })
 
   next()
 })
